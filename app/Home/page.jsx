282a75c2c9a543a9bdc8abc3cffd091ee5585b19
Malik Zohaib Zahid <64071105+Malik-Zohaib-Debug@users.noreply.github.com>
2023-12-30 16:13:24 +0500

    initial commit

---
+++ app/Home/page.jsx
@@ -0,0 +1,12 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <Flex>
+      <Text>Home Page</Text>
+    </Flex>
+  )
+}
+
+export default HomePage;
