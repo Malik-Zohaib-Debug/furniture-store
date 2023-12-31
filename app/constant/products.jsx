2d20388c00f0be1c5a700ab126f4d6b54c8f8535
Malik Zohaib Zahid <64071105+Malik-Zohaib-Debug@users.noreply.github.com>
2023-12-31 22:04:01 +0500

    added static data

---
+++ app/constant/products.jsx
@@ -0,0 +1,228 @@
+const data = [
+    {
+        id: 1,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product1.svg"
+    },
+    {
+        id: 2,
+        title: 'Leviosa',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product2.svg"
+    },
+    {
+        id: 3,
+        title: 'Lolito',
+        price: 7000,
+        desc: "Outdoor bar table and stool",
+        image: "/home/product3.svg"
+    },
+    {
+        id: 4,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product4.svg"
+    },
+    {
+        id: 5,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product5.svg"
+    },
+    {
+        id: 6,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product6.svg"
+    },
+    {
+        id: 7,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product7.svg"
+    },
+    {
+        id: 8,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product1.svg"
+    },
+    {
+        id: 9,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product2.svg"
+    },
+    {
+        id: 10,
+        title: 'Leviosa',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product3.svg"
+    },
+    {
+        id: 11,
+        title: 'Lolito',
+        price: 7000,
+        desc: "Outdoor bar table and stool",
+        image: "/home/product4.svg"
+    },
+    {
+        id: 12,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product5.svg"
+    },
+    {
+        id: 13,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product6.svg"
+    },
+    {
+        id: 14,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product7.svg"
+    },
+    {
+        id: 15,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product8.svg"
+    },
+    {
+        id: 16,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product1.svg"
+    },
+    {
+        id: 17,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product2.svg"
+    },
+    {
+        id:18,
+        title: 'Leviosa',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product3.svg"
+    },
+    {
+        id: 19,
+        title: 'Lolito',
+        price: 7000,
+        desc: "Outdoor bar table and stool",
+        image: "/home/product4.svg"
+    },
+    {
+        id: 20,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product5.svg"
+    },
+    {
+        id: 21,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product6.svg"
+    },
+    {
+        id: 22,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product7.svg"
+    },
+    {
+        id: 23,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product8.svg"
+    },
+    {
+        id: 24,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product1.svg"
+    },
+    {
+        id: 25,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product2.svg"
+    },
+    {
+        id: 26,
+        title: 'Leviosa',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product3.svg"
+    },
+    {
+        id: 27,
+        title: 'Lolito',
+        price: 7000,
+        desc: "Outdoor bar table and stool",
+        image: "/home/product4.svg"
+    },
+    {
+        id: 28,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product5.svg"
+    },
+    {
+        id: 29,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product6.svg"
+    },
+    {
+        id: 30,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product7.svg"
+    },
+    {
+        id: 31,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product8.svg"
+    },
+    {
+        id: 32,
+        title: 'Syltherine',
+        price: 2500,
+        desc: "stylish cafe chair",
+        image: "/home/product1.svg"
+    },
+];
+
+export default data;
